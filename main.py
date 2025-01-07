48868b54f8102b01283b1c5246141346e2788634
scjones02010 <scjones02010@gmail.com>
2025-01-07 18:51:19 -0500

    add timeout

--- main.py
+++ main.py
@@ -3,9 +3,25 @@ from constants import *
 
 def main():
     pygame.init()
+    clock= pygame.time.Clock()
+    dt=0
     print("Starting asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    running= True
+    while running:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                running=False
+        clock.tick(60)
+        dt= (dt - clock.tick())/1000  
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+
     return
 
 if __name__=='__main__':
