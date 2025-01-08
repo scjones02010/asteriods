151a2c29dbad51f6e8be95c8b12f100913f4536e
scjones02010 <scjones02010@gmail.com>
2025-01-07 22:30:55 -0500

    add player to game

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     clock= pygame.time.Clock()
     dt=0
+    player=Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print("Starting asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
@@ -15,13 +17,11 @@ def main():
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 running=False
-        clock.tick(60)
-        dt= (dt - clock.tick())/1000  
+        tick = clock.tick(60)
+        dt= tick/1000
         screen.fill((0,0,0))
+        player.draw(screen)  
         pygame.display.flip()
-
-
-
     return
 
 if __name__=='__main__':
