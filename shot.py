0e3cdd8cd3408415cae50245f7e51f852ffd0d3e
scjones02010 <scjones02010@gmail.com>
2025-01-23 21:56:58 -0500

    space typo

--- shot.py
+++ shot.py
@@ -10,5 +10,5 @@ class Shot(CircleShape):
     def update(self,dt):
         self.position+=self.velocity*dt
 
-def draw(self, screen):
-    pygame.draw.circle(screen, 'white', self.position, self.radius)
+    def draw(self, screen):
+        pygame.draw.circle(screen, 'white', self.position, self.radius)
